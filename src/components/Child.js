432dcab3791866fa41e6ff7ea7e5ceab7e696ec7
shrutijain2296 <shrutijain2296@gmail.com>
2023-07-19 17:27:20 +0530

    first commit

--- src/components/Child.js
+++ src/components/Child.js
@@ -7,7 +7,13 @@ const Child = ({showModal, btnClicked}) =>{
             <h2>Child Component</h2>
             <button onClick={btnClicked}>Show Modal</button>
             {
-                showModal && <h3>Modal Content</h3> && <p>This is the modal content.</p>
+                showModal && (
+                    <div>
+                        <h3>Modal Content</h3>
+                        <p>This is the modal content.</p>
+                    </div>
+                )
+                
             }
         </div>
     )
